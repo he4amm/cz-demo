8dc7d930f3b9251d4f60f104ad0eaa2931f0bfce
he4amm <he4amm@gmail.com>
2019-03-31 17:34:13 +0200

    Updates

---
+++ precache-manifest.df9c523d22e8c921513b7de5a9b3bde0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "c49839ece52709184cff90dffc00f6da",
+    "url": "/cz-demo/static/media/oval.c49839ec.png"
+  },
+  {
+    "revision": "80d0190ca6b8510dcb9e779960594057",
+    "url": "/cz-demo/static/media/house.80d0190c.svg"
+  },
+  {
+    "revision": "3c9b5974c303913b45683d8624004e7c",
+    "url": "/cz-demo/static/media/accordion_plus.3c9b5974.svg"
+  },
+  {
+    "revision": "c8e3c4fc1d2d349cb422f10e4d952845",
+    "url": "/cz-demo/static/media/accordion_minus.c8e3c4fc.svg"
+  },
+  {
+    "revision": "c839ea8c953751e53274",
+    "url": "/cz-demo/static/js/runtime~main.6f6ee0cc.js"
+  },
+  {
+    "revision": "b0594234f64b05ac58ad",
+    "url": "/cz-demo/static/js/main.5484eb31.chunk.js"
+  },
+  {
+    "revision": "343cd20241344a0a9611",
+    "url": "/cz-demo/static/js/2.868ba8de.chunk.js"
+  },
+  {
+    "revision": "b0594234f64b05ac58ad",
+    "url": "/cz-demo/static/css/main.ec0dd30b.chunk.css"
+  },
+  {
+    "revision": "63971c3a19afa08a388d2680ce5f608a",
+    "url": "/cz-demo/index.html"
+  }
+];
